d7693ac7740679f67373d1ac55c55ad6b71f0a98
Tim Wilson <twilsonco@gmail.com>
2021-09-17 16:40:04 -0600

    implement improved sigmoid steer from https://github.com/qadmus/openpilot/commit/41cb494aa4373392bef9cba5e60bf5c5dbca50d9#diff-9a1a1717b18a0db2d2de5bcc958b3ce977f91374a5f5ed7a6b1653ed0cab83b6

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -69,12 +69,12 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 0.45 * ret.wheelbase # from Volt Gen 1
 
       ret.lateralTuning.pid.kpBP = [0., 40.]
-      ret.lateralTuning.pid.kpV = [0., 0.2]
+      ret.lateralTuning.pid.kpV = [0., 0.19]
       ret.lateralTuning.pid.kiBP = [0.]
-      ret.lateralTuning.pid.kiV = [0.005]
+      ret.lateralTuning.pid.kiV = [0.0, 0.01]
       ret.lateralTuning.pid.kf = 0.1 # !!! ONLY for sigmoid feedforward !!!
-      ret.steerRateCost = 0.5
-      ret.steerActuatorDelay = 0.21
+      ret.steerRateCost = 1.0
+      ret.steerActuatorDelay = 0.18
 
       # Only tuned to reduce oscillations. TODO.
       ret.longitudinalTuning.kpV = [1.7, 1.3]
