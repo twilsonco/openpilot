c3c2adf38a71a9da459b5c58a46c3d214926a2b3
Tim Wilson <twilsonco@gmail.com>
2021-09-21 13:51:33 -0600

    use negative acceleration profiles

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -30,7 +30,7 @@ AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distract
 _A_CRUISE_MIN_V_SPORT = [-3.0, -2.6, -2.3, -2.0, -1.0]
 _A_CRUISE_MIN_V_FOLLOWING = [-3.0, -2.5, -2.0, -1.5, -1.0]
 _A_CRUISE_MIN_V = [-2.0, -1.5, -1.0, -0.7, -0.5]
-_A_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 55.0]
+_A_CRUISE_MIN_BP = [0., 5., 10., 20., 55.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
@@ -39,6 +39,7 @@ _A_CRUISE_MAX_V_SPORT = [3.0, 3.2, 3.0, 2.0, 2.0]
 _A_CRUISE_MAX_V_FOLLOWING = [2.0, 1.8, 2.0, .6, .4]
 _A_CRUISE_MAX_BP = [0., 5., 10., 20., 55.]
 
+_A_CRUISE_MIN_V_MODE_LIST = [_A_CRUISE_MIN_V, _A_CRUISE_MIN_V_SPORT]
 _A_CRUISE_MAX_V_MODE_LIST = [_A_CRUISE_MAX_V, _A_CRUISE_MAX_V_SPORT]
 
 # Lookup table for turns - fast accel
@@ -50,10 +51,11 @@ _A_TOTAL_MAX_V_SOC = [1.7, 3.2]
 _A_TOTAL_MAX_BP_SOC = [20., 40.]
 
 def calc_cruise_accel_limits(v_ego, following, accelMode):
-  a_cruise_min = interp(v_ego, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V)
   if following:
+    a_cruise_min = interp(v_ego, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V_FOLLOWING)
     a_cruise_max = interp(v_ego, _A_CRUISE_MAX_BP, _A_CRUISE_MAX_V_FOLLOWING)
   else:
+    a_cruise_min = interp(v_ego, _A_CRUISE_MIN_BP, _A_CRUISE_MIN_V_MODE_LIST[accelMode])
     a_cruise_max = interp(v_ego, _A_CRUISE_MAX_BP, _A_CRUISE_MAX_V_MODE_LIST[accelMode])
   return [a_cruise_min, a_cruise_max]
 
