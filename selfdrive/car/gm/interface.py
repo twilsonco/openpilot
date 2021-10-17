60943a580e19bec7ef997cc53d2c285462f05d05
Tim Wilson <twilsonco@gmail.com>
2021-10-17 12:33:48 -0600

    adjust volt ki re https://github.com/qadmus/openpilot/commit/636988f240695f1dd80e54d73fa493db51955183

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -84,7 +84,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 40.]
       ret.lateralTuning.pid.kpV = [0., 0.16]
       ret.lateralTuning.pid.kiBP = [0.]
-      ret.lateralTuning.pid.kiV = [0.1]
+      ret.lateralTuning.pid.kiV = [0.01]
       ret.lateralTuning.pid.kf = 1. # !!! ONLY for sigmoid feedforward !!!
       ret.steerActuatorDelay = 0.2
 
