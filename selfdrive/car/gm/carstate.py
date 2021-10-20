fb06acd3aa38e8a3e6380e6074bb9f22ad9369dc
Tim Wilson <twilsonco@gmail.com>
2021-10-20 07:58:10 -0600

    lower minimum one-pedal engage on gas speed to 1

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -65,7 +65,7 @@ class CarState(CarStateBase):
     self.one_pedal_mode_op_braking_allowed = not self._params.get_bool("OnePedalModeSimple")
     self.one_pedal_mode_engage_on_gas_enabled = self._params.get_bool("OnePedalModeEngageOnGas") and (self.one_pedal_mode_enabled or (not self.disengage_on_gas and self.coasting_enabled))
     self.one_pedal_mode_engage_on_gas = False
-    self.one_pedal_mode_engage_on_gas_min_speed = 7. * CV.MPH_TO_MS
+    self.one_pedal_mode_engage_on_gas_min_speed = 1. * CV.MPH_TO_MS
     self.one_pedal_mode_max_set_speed = 5 * CV.MPH_TO_MS #  one pedal mode activates if cruise set at or below this speed
     self.one_pedal_mode_stop_apply_brake_bp = [[i * CV.MPH_TO_MS for i in [0., 1., 4., 45., 85.]], [i * CV.MPH_TO_MS for i in [0., 1., 4., 45., 85.]], [1.]]
     self.one_pedal_mode_stop_apply_brake_v = [[70., 70., 95., 115., 90.], [100., 100., 160., 180., 120.], [250.]] # three levels. 1-2 are cycled using follow distance press, and 3 by holding
