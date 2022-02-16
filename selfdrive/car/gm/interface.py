79a1504d0c5eab7173ec7db0fa66c6a92850c056
Tim Wilson <twilsonco@gmail.com>
2022-02-15 18:57:05 -0700

    volt lateral tuning

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -136,9 +136,9 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 0.45 * ret.wheelbase # from Volt Gen 1
 
       ret.lateralTuning.pid.kpBP = [0., 40.]
-      ret.lateralTuning.pid.kpV = [0.0, 0.17]
+      ret.lateralTuning.pid.kpV = [0.0, 0.19]
       ret.lateralTuning.pid.kiBP = [i * CV.MPH_TO_MS for i in [0., 15., 55., 80.]]
-      ret.lateralTuning.pid.kiV = [0., .015, .01, .002]
+      ret.lateralTuning.pid.kiV = [0., .018, .012, .01]
       ret.lateralTuning.pid.kdV = [0.08]
       ret.lateralTuning.pid.kf = 1. # !!! ONLY for sigmoid feedforward !!!
       ret.steerActuatorDelay = 0.2
