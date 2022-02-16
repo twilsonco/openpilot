59c5d04b5a199b0b7d1e5d9d0f0ea49d84bdfcb9
Tim Wilson <twilsonco@gmail.com>
2022-02-16 08:17:46 -0700

    volt lower steer rate cost

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -130,7 +130,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1607. + STD_CARGO_KG
       ret.wheelbase = 2.69
       ret.steerRatio = 17.7  # Stock 15.7, LiveParameters
-      ret.steerRateCost = 0.9
+      ret.steerRateCost = 0.8
       tire_stiffness_factor = 0.469 # Stock Michelin Energy Saver A/S, LiveParameters
       ret.steerRatioRear = 0.
       ret.centerToFront = 0.45 * ret.wheelbase # from Volt Gen 1
