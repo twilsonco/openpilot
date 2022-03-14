3d45c10172d7cf3f8d6e787f9dc580561f104535
Tim Wilson <twilsonco@gmail.com>
2022-03-13 23:37:57 -0600

    vision turn controller fiddling part 2

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -14,10 +14,10 @@ _MIN_V = 5.6  # Do not operate under 20km/h
 _ENTERING_PRED_LAT_ACC_TH = 1.3  # Predicted Lat Acc threshold to trigger entering turn state.
 _ABORT_ENTERING_PRED_LAT_ACC_TH = 1.1  # Predicted Lat Acc threshold to abort entering state if speed drops.
 
-_TURNING_LAT_ACC_TH = 2.2  # Lat Acc threshold to trigger turning turn state.
+_TURNING_LAT_ACC_TH = 2.0  # Lat Acc threshold to trigger turning turn state.
 
-_LEAVING_LAT_ACC_TH = 1.9  # Lat Acc threshold to trigger leaving turn state.
-_FINISH_LAT_ACC_TH = 1.7  # Lat Acc threshold to trigger end of turn cycle.
+_LEAVING_LAT_ACC_TH = 1.3  # Lat Acc threshold to trigger leaving turn state.
+_FINISH_LAT_ACC_TH = 1.1  # Lat Acc threshold to trigger end of turn cycle.
 
 _EVAL_STEP = 5.  # mts. Resolution of the curvature evaluation.
 _EVAL_START = 20.  # mts. Distance ahead where to start evaluating vision curvature.
@@ -33,8 +33,8 @@ _ENTERING_SMOOTH_DECEL_BP = [1.3, 3]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.5, 0., -0.4]  # acc value
-_TURNING_ACC_BP = [1.5, 2.3, 3.]  # absolute value of current lat acc
+_TURNING_ACC_V = [0.7, 0., -0.6]  # acc value
+_TURNING_ACC_BP = [1.1, 2.3, 3.8]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.5  # Confortble acceleration to regain speed while leaving a turn.
 
