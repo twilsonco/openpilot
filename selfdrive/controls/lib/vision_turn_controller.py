6b76815a0a7cd43e4469d155d3566c9c5c14eafd
Tim Wilson <twilsonco@gmail.com>
2022-03-24 11:34:19 -0600

    vision turn controller: higher accel based on lateral accel

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -33,8 +33,8 @@ _ENTERING_SMOOTH_DECEL_BP = [1.3, 3]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.9, 0., -0.6]  # acc value
-_TURNING_ACC_BP = [1.1, 2.3, 3.8]  # absolute value of current lat acc
+_TURNING_ACC_V = [0.9, 0., -0.8]  # acc value
+_TURNING_ACC_BP = [1.4, 2.5, 4.1]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.5  # Confortble acceleration to regain speed while leaving a turn.
 
