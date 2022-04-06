f704c9855e4f5f29e97791ddbfa3abc22d579a9f
Tim Wilson <twilsonco@gmail.com>
2022-04-06 07:37:43 -0600

    Volt lower lowspeed long ki

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -140,7 +140,7 @@ class CarInterface(CarInterfaceBase):
       # Only tuned to reduce oscillations. TODO.
       ret.longitudinalTuning.kpV = [1.6, 1.3]
       ret.longitudinalTuning.kiBP = [5., 35.]
-      ret.longitudinalTuning.kiV = [0.27, 0.34]
+      ret.longitudinalTuning.kiV = [0.25, 0.34]
       ret.longitudinalTuning.kdV = [0.8, 0.4]
       ret.longitudinalTuning.kdBP = [5., 25.]
 
