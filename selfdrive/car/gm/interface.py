7284387c4684f8f7439038f6cece143fe6f55db3
Tim Wilson <twilsonco@gmail.com>
2022-04-19 09:11:48 -0600

    bring in current acadia tune

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -169,18 +169,19 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.86
       ret.steerRatio = 16.0 #14.4  # end to end is 13.46
       ret.steerRatioRear = 0.
-      ret.steerActuatorDelay = 0.4
       ret.centerToFront = ret.wheelbase * 0.4
+      ret.steerActuatorDelay = 0.24
       ret.lateralTuning.pid.kpBP = [i * CV.MPH_TO_MS for i in [0., 80.]]
       ret.lateralTuning.pid.kpV = [0., 0.2]
       ret.lateralTuning.pid.kiBP = [0.0]
       ret.lateralTuning.pid.kiV = [0.025]
-      ret.lateralTuning.pid.kdV = [0.35]
+      ret.lateralTuning.pid.kdBP = [i * CV.MPH_TO_MS for i in [15., 30., 55.]]
+      ret.lateralTuning.pid.kdV = [0.1, 0.28, 0.32]
       ret.lateralTuning.pid.kf = 1. # get_steer_feedforward_acadia()
       ret.longitudinalTuning.kdBP = [5., 25.]
-      ret.longitudinalTuning.kdV = [0.8, 0.2]
+      ret.longitudinalTuning.kdV = [0.8, 0.4]
       ret.longitudinalTuning.kiBP = [5., 35.]
-      ret.longitudinalTuning.kiV = [0.3, 0.34]
+      ret.longitudinalTuning.kiV = [0.31, 0.34]
 
     elif candidate == CAR.BUICK_REGAL:
       ret.minEnableSpeed = 18 * CV.MPH_TO_MS
