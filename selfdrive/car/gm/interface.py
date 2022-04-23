e0cd73741a78102885fe7e2b74a46d5f8c3bee33
Tim Wilson <twilsonco@gmail.com>
2022-04-23 10:02:38 -0600

    volt lat lower ki
    volt lat: lower ki
    
    volt lat: higher kd

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -132,9 +132,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 40.]
       ret.lateralTuning.pid.kpV = [0., .16]
       ret.lateralTuning.pid.kiBP = [0.]
-      ret.lateralTuning.pid.kiV = [.035]
+      ret.lateralTuning.pid.kiV = [.028]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [.5]
+      ret.lateralTuning.pid.kdV = [.6]
       ret.lateralTuning.pid.kf = 1. # !!! ONLY for sigmoid feedforward !!!
       ret.steerActuatorDelay = 0.18
 
