3afaac4d03a152412f80d8142e637c1275da818d
Tim Wilson <twilsonco@gmail.com>
2022-04-29 23:43:28 -0600

    apply brian's changes

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -282,7 +282,7 @@ class CarInterface(CarInterfaceBase):
         be.pressed = False
         but = self.CS.prev_cruise_buttons
       if but == CruiseButtons.RES_ACCEL:
-        if not (ret.cruiseState.enabled and ret.standstill):
+        if not (ret.cruiseState.enabled and ret.standstill) or self.CS.no_acc:
           be.type = ButtonType.accelCruise  # Suppress resume button if we're resuming from stop so we don't adjust speed.
       elif but == CruiseButtons.DECEL_SET:
         if not cruiseEnabled and not self.CS.lkMode:
--- selfdrive/car/gm/radar_interface.py
+++ selfdrive/car/gm/radar_interface.py
@@ -108,4 +108,60 @@ class RadarInterface(RadarInterfaceBase):
     return ret
 
 class RadarlessInterface(RadarInterfaceBase):
-  pass
+  def __init__(self, CP):
+    super().__init__(CP)
+
+    self.rcp = create_radar_can_parser(CP.carFingerprint)
+
+    self.trigger_msg = LAST_RADAR_MSG
+    self.updated_messages = set()
+    self.radar_ts = CP.radarTimeStep
+
+  def update(self, can_strings):
+    if self.rcp is None:
+      return super().update(None)
+
+    vls = self.rcp.update_strings(can_strings)
+    self.updated_messages.update(vls)
+
+    if self.trigger_msg not in self.updated_messages:
+      return None
+
+    ret = car.RadarData.new_message()
+    header = self.rcp.vl[RADAR_HEADER_MSG]
+
+    currentTargets = set()
+    num_targets = header['FLRRNumValidTargets']
+
+    # Not all radar messages describe targets,
+    # no need to monitor all of the self.rcp.msgs_upd
+    for ii in self.updated_messages:
+      if ii == RADAR_HEADER_MSG:
+        continue
+
+      if num_targets == 0:
+        break
+
+      cpt = self.rcp.vl[ii]
+      # Zero distance means it's an empty target slot
+      if cpt['TrkRange'] > 0.0:
+        targetId = cpt['TrkObjectID']
+        currentTargets.add(targetId)
+        if targetId not in self.pts:
+          self.pts[targetId] = car.RadarData.RadarPoint.new_message()
+          self.pts[targetId].trackId = targetId
+        distance = cpt['TrkRange']
+        self.pts[targetId].dRel = distance  # from front of car
+        # From driver's pov, left is positive
+        self.pts[targetId].yRel = math.sin(cpt['TrkAzimuth'] * CV.DEG_TO_RAD) * distance
+        self.pts[targetId].vRel = cpt['TrkRangeRate']
+        self.pts[targetId].aRel = float('nan')
+        self.pts[targetId].yvRel = float('nan')
+
+    for oldTarget in list(self.pts.keys()):
+      if oldTarget not in currentTargets:
+        del self.pts[oldTarget]
+
+    ret.points = list(self.pts.values())
+    self.updated_messages.clear()
+    return ret
