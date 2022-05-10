1490d8942dd48ab65ce63a990c179f1609e0d1f5
Tim Wilson <twilsonco@gmail.com>
2022-05-09 20:41:36 -0600

    Volt torque lat: increase kf

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -136,7 +136,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.torque.kp = 2.0 / max_torque
         ret.lateralTuning.torque.ki = 0.4 / max_torque
         ret.lateralTuning.torque.kd = 3.0 / max_torque
-        ret.lateralTuning.torque.kf = 1.3 / max_torque
+        ret.lateralTuning.torque.kf = 1.4 / max_torque
         ret.lateralTuning.torque.friction = 0.02
       else:
         ret.lateralTuning.pid.kpBP = [0., 40.]
