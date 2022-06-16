563d27a607bca91558a2b8e1c0f7d55bbd2432b4
Tim Wilson <twilsonco@gmail.com>
2022-06-16 16:12:05 -0600

    vision braking tuning

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -32,12 +32,12 @@ _A_LAT_REG_MAX = 2.5  # Maximum lateral acceleration
 # Lookup table for the minimum smooth deceleration during the ENTERING state
 # depending on the actual maximum absolute lateral acceleration predicted on the turn ahead.
 _ENTERING_SMOOTH_DECEL_V = [0.0, -0.2, -1.]  # min decel value allowed on ENTERING state
-_ENTERING_SMOOTH_DECEL_BP = [1.3, 1.9, 3.5]  # absolute value of lat acc ahead
+_ENTERING_SMOOTH_DECEL_BP = [1.3, 1.8, 3.8]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
 _TURNING_ACC_V = [0.6, 0.0, -1.]  # acc value
-_TURNING_ACC_BP = [1.3, 2.5, 3.5]  # absolute value of current lat acc
+_TURNING_ACC_BP = [1.5, 2.45, 3.8]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.6  # Confortble acceleration to regain speed while leaving a turn.
 
