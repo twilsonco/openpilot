bbe6025fd9e003ddea9c5f5b1e1b723743e3b17f
Tim Wilson <twilsonco@gmail.com>
2022-07-09 14:40:08 -0600

    vision braking: slightly slower in turning state

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -38,7 +38,7 @@ _ENTERING_SMOOTH_DECEL_BP = [1.2, 1.65, 3.6]  # absolute value of lat acc ahead
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
 _TURNING_ACC_V = [0.6, 0.0, -1.]  # acc value
-_TURNING_ACC_BP = [1.3, 2.57, 3.4]  # absolute value of current lat acc
+_TURNING_ACC_BP = [1.3, 2.45, 3.2]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.6  # Confortble acceleration to regain speed while leaving a turn.
 
