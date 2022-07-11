ff5d0c94fdd6a28d78cc5bfa4f42e824870bbf71
Tim Wilson <twilsonco@gmail.com>
2022-07-10 18:08:13 -0600

    vision braking: slower in curves

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -33,12 +33,12 @@ _A_LAT_REG_MAX = 2.25  # Maximum lateral acceleration
 # Lookup table for the minimum smooth deceleration during the ENTERING state
 # depending on the actual maximum absolute lateral acceleration predicted on the turn ahead.
 _ENTERING_SMOOTH_DECEL_V = [0.0, -0.2, -1.5]  # min decel value allowed on ENTERING state
-_ENTERING_SMOOTH_DECEL_BP = [1.2, 1.65, 3.6]  # absolute value of lat acc ahead
+_ENTERING_SMOOTH_DECEL_BP = [1.2, 1.65, 3.5]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.6, 0.0, -1.]  # acc value
-_TURNING_ACC_BP = [1.3, 2.45, 3.2]  # absolute value of current lat acc
+_TURNING_ACC_V = [0.4, 0.0, -1.]  # acc value
+_TURNING_ACC_BP = [1.3, 2.3, 3.0]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.6  # Confortble acceleration to regain speed while leaving a turn.
 
