948eef35aa707533fc9dd293a48fc109618bcf88
Tim Wilson <twilsonco@gmail.com>
2022-07-14 18:49:48 -0600

    Vision braking: slower in curves and more low speed boost

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -38,7 +38,7 @@ _ENTERING_SMOOTH_DECEL_BP = [1.2, 1.65, 3.5]  # absolute value of lat acc ahead
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
 _TURNING_ACC_V = [0.5, 0.0, -1.]  # acc value
-_TURNING_ACC_BP = [1.3, 2.3, 3.0]  # absolute value of current lat acc
+_TURNING_ACC_BP = [1.3, 2.2, 3.0]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.6  # Confortable acceleration to regain speed while leaving a turn.
 
@@ -46,7 +46,7 @@ _MIN_LANE_PROB = 0.6  # Minimum lanes probability to allow curvature prediction 
 
 # scale velocity used to determine curvature in order to provide more braking at low speed
 # where the LKA torque is less capable despite low lateral acceleration.
-_LOW_SPEED_SCALE_V = [1.07, 1.0] #increase the first value to increase low-speed vision braking; don't touch the second
+_LOW_SPEED_SCALE_V = [1.1, 1.0] #increase the first value to increase low-speed vision braking; don't touch the second
 _LOW_SPEED_SCALE_BP = [i * CV.MPH_TO_MS for i in [0., 30.]]
 
 # scale up current measured lateral acceleration if the lateral controller is saturated
