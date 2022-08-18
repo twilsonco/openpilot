a873babb9e7cc5705533d8952e3c08224c03a35a
Tim Wilson <twilsonco@gmail.com>
2022-08-18 10:06:34 -0600

    gm raise steer limits

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -9,8 +9,8 @@ class CarControllerParams():
   def __init__(self):
     self.STEER_MAX = 300
     self.STEER_STEP = 2              # how often we update the steer cmd
-    self.STEER_DELTA_UP = 5          # ~1s time to peak torque (255/50hz/1s)
-    self.STEER_DELTA_DOWN = 13       # ~0.4s from peak torque to zero
+    self.STEER_DELTA_UP = 14          # ~1s time to peak torque (255/50hz/1s)
+    self.STEER_DELTA_DOWN = 34       # ~0.4s from peak torque to zero
     self.MIN_STEER_SPEED = 3.
     self.STEER_DRIVER_ALLOWANCE = 50   # allowed driver torque before start limiting
     self.STEER_DRIVER_MULTIPLIER = 4   # weight driver torque heavily
