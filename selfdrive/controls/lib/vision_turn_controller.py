b700ccdb0eea072be46948df745048127d15a175
Tim Wilson <twilsonco@gmail.com>
2022-08-22 18:08:07 -0600

    Vision braking more entering brake;
    more decel available; align entering and turning brake profiles
    
    Vision braking Faster in curves
    
    Vision slight faster in curves
    
    Vision faster in curves, but same at low speed

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -27,17 +27,17 @@ _EVAL_START = 20.  # mts. Distance ahead where to start evaluating vision curvat
 _EVAL_LENGHT = 150.  # mts. Distance ahead where to stop evaluating vision curvature.
 _EVAL_RANGE = np.arange(_EVAL_START, _EVAL_LENGHT, _EVAL_STEP)
 
-_A_LAT_REG_MAX = 1.9  # Maximum lateral acceleration
+_A_LAT_REG_MAX = 1.8  # Maximum lateral acceleration
 
 # Lookup table for the minimum smooth deceleration during the ENTERING state
 # depending on the actual maximum absolute lateral acceleration predicted on the turn ahead.
-_ENTERING_SMOOTH_DECEL_V = [0.0, -0.2, -1.5]  # min decel value allowed on ENTERING state
+_ENTERING_SMOOTH_DECEL_V = [0.0, -0.2, -2.0]  # min decel value allowed on ENTERING state
 _ENTERING_SMOOTH_DECEL_BP = [1.2, 1.4, 3.5]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.6, 0.0, -1.]  # acc value
-_TURNING_ACC_BP = [0.8, 1.3, 2.2]  # absolute value of current lat acc
+_TURNING_ACC_V = [0.6, 0.0, -1.5]  # acc value
+_TURNING_ACC_BP = [0.8, 1.5, 2.2]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.7  # Confortable acceleration to regain speed while leaving a turn.
 
