606ceed9553a6dc50ff4a6551f26bee4d8ecd2b3
Tim Wilson <twilsonco@gmail.com>
2022-08-24 08:04:14 -0600

    Volt lat raise kd

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -179,7 +179,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.torque.useSteeringAngle = True
         ret.lateralTuning.torque.kp = 1.8 / max_lateral_accel
         ret.lateralTuning.torque.ki = 0.55 / max_lateral_accel
-        ret.lateralTuning.torque.kd = 5.0 / max_lateral_accel
+        ret.lateralTuning.torque.kd = 6.0 / max_lateral_accel
         ret.lateralTuning.torque.kf = 1.0 # use with custom torque ff
         ret.lateralTuning.torque.friction = 0.005
       else:
