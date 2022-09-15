0aa0b514c42f290347449447ee8b023d3360f4f2
Tim Wilson <twilsonco@gmail.com>
2022-09-15 08:19:53 -0600

    One pedal adjust mixing of old and new one pedal brake values

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -34,6 +34,7 @@ ONE_PEDAL_MODE_DECEL_V = [
 ] # light, medium, and hard one-pedal braking
 ONE_PEDAL_MIN_SPEED = 2.5
 ONE_PEDAL_DECEL_RATE_FACTOR = 0.9 * DT_CTRL * 4 # multiplied by one pedal decel to determine rate
+ONE_PEDAL_DECEL_RATE_K = 0.75 # factor weighting the current one pedal accel over the old
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
@@ -170,7 +171,7 @@ class CarController():
               one_pedal_apply_brake_decel_minus1 = interp(CS.vEgo, ONE_PEDAL_MODE_DECEL_BP[max(0,CS.one_pedal_brake_mode-1)], ONE_PEDAL_MODE_DECEL_V[max(0,CS.one_pedal_brake_mode-1)])
               one_pedal_decel = interp(abs(CS.angle_steers), CS.one_pedal_angle_steers_cutoff_bp, [one_pedal_decel, one_pedal_apply_brake_decel_minus1])
 
-            rate_limit = abs(ONE_PEDAL_DECEL_RATE_FACTOR * 0.5 * (one_pedal_decel + self.one_pedal_decel_in))
+            rate_limit = abs(ONE_PEDAL_DECEL_RATE_FACTOR * (ONE_PEDAL_DECEL_RATE_K * one_pedal_decel + (1 - ONE_PEDAL_DECEL_RATE_K) * self.one_pedal_decel_in))
             self.one_pedal_decel_in = clip(one_pedal_decel, self.one_pedal_decel_in - rate_limit, self.one_pedal_decel_in + rate_limit)
             
 
