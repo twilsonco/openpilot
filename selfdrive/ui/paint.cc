43214a65b72cc72d91ded405ad4d95170b828576
Tim Wilson <twilsonco@gmail.com>
2022-09-17 16:26:09 -0600

    dot

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1877,7 +1877,7 @@ static void ui_draw_measures(UIState *s){
             {
               snprintf(name, sizeof(name), "EV CON NOW");
               if (scene.car_state.getVEgo() > 0.1){
-                float temp = scene.ev_recip_eff_wa[0].;
+                float temp = scene.ev_recip_eff_wa[0];
                 if (abs(temp) >= 10.){
                   snprintf(val, sizeof(val), "%.0f", temp);
                 }
@@ -1895,7 +1895,7 @@ static void ui_draw_measures(UIState *s){
           case UIMeasure::EV_CONSUM_RECENT: 
             {
               snprintf(name, sizeof(name), (scene.is_metric ? "EV CON 8km" : "EV CON 5mi"));
-              float temp = scene.ev_recip_eff_wa[1].;
+              float temp = scene.ev_recip_eff_wa[1];
               if (abs(temp) >= 100.){
                 snprintf(val, sizeof(val), "%.0f", temp);
               }
