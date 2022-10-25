13d2a3c168e90203cc1b47d9ef8f922ea0f11ab6
Tim Wilson <twilsonco@gmail.com>
2022-10-25 08:14:26 -0600

    lead+1 lower follow distance buffer

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -65,7 +65,7 @@ def calc_cruise_accel_limits(v_ego, following, accelMode):
     a_cruise_max = interp(v_ego, _A_CRUISE_MAX_BP, _A_CRUISE_MAX_V_MODE_LIST[accelMode])
   return [a_cruise_min, a_cruise_max]
 
-LEAD_ONE_PLUS_TR_BUFFER = 0.8 # [s] follow distance between lead and lead+1 to run the lead+1 mpc
+LEAD_ONE_PLUS_TR_BUFFER = 0.4 # [s] follow distance between lead and lead+1 to run the lead+1 mpc
 
 def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
   """
