8449e72c108e0a7b5c070e64c087d80e55d185ad
Tim Wilson <twilsonco@gmail.com>
2022-11-14 10:44:58 -0700

    Vision breaking: faster filter

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -113,7 +113,7 @@ class VisionTurnController():
     self._v_cruise_setpoint = 0.
     self._v_ego = 0.
     self._a_ego = 0.
-    self._a_target = FirstOrderFilter(0., 1.0, DT_CTRL)
+    self._a_target = FirstOrderFilter(0., 0.5, DT_CTRL)
     self._v_overshoot = 0.
     self._state = VisionTurnControllerState.disabled
     self._CS = None
