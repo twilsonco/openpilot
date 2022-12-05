e6b70e52e8bbf2ba16224eb79b75cbfb789faf64
Tim Wilson <twilsonco@gmail.com>
2022-12-04 18:00:30 -0700

    increase friction threshold and volt friction

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -17,7 +17,7 @@ from cereal import log
 # move it at all, this is compensated for too.
 
 
-FRICTION_THRESHOLD = 0.3
+FRICTION_THRESHOLD = 1.5
 
 def get_steer_feedforward(desired_lateral_accel, speed):
   return desired_lateral_accel
