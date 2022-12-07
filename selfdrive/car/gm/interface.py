9069d316cc1eddacac1340c056ac8bddfdbf3233
Tim Wilson <twilsonco@gmail.com>
2022-12-07 11:29:07 -0700

    no custom bolt tune for now. uses bolt euv

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -125,8 +125,8 @@ class CarInterface(CarInterfaceBase):
   def torque_from_lateral_accel(self) -> TorqueFromLateralAccelCallbackType:
     if self.CP.carFingerprint == CAR.VOLT:
       return self.torque_from_lateral_accel_volt
-    elif self.CP.carFingerprint == CAR.BOLT_EV:
-      return self.torque_from_lateral_accel_bolt
+    # elif self.CP.carFingerprint == CAR.BOLT_EV:
+    #   return self.torque_from_lateral_accel_bolt
     elif self.CP.carFingerprint == CAR.BOLT_EUV:
       return self.torque_from_lateral_accel_bolt_euv
     else:
