393f38567ce99d94422155a92711fad3b9b3a58c
Tim Wilson <twilsonco@gmail.com>
2023-01-10 18:25:29 -0700

    allow MADS one-pedal and lead braking while regen paddle pressed
    better condition
    
    corresponding panda changes
    
    one pedal correct error
    
    one pedal allow for regen paddle "gear shifter"
    
    opParams: adjust defaults for one-pedal one-time stop
    
    verbiage
    
    again
    
    ev gear shifter 2 underscore
    
    opParams: positive accel smoothing
    
    log gas brake threshold accel
    
    negate logged apply brake
    
    raise one-pedal min speed
    
    fix cereal ordinal
    
    one-pedal live update long tune
    
    fix my "fixing" of one-pedal error

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -955,10 +955,11 @@ class Controls:
     controlsState.distractionTimer = int(self.distraction_timer)
     
     controlsState.applyGas = int(self.CI.CC.apply_gas)
-    controlsState.applyBrakeOut = int(self.CI.CC.apply_brake_out)
-    controlsState.applyBrakeIn = int(self.CI.CC.apply_brake_in)
+    controlsState.applyBrakeOut = int(-self.CI.CC.apply_brake_out)
+    controlsState.applyBrakeIn = int(-self.CI.CC.apply_brake_in)
     controlsState.applySteer = int(self.CI.CC.apply_steer)
     controlsState.brakesAllowed = bool(self.CI.CC.brakes_allowed)
+    controlsState.gasBrakeThresholdAccel = float(self.CI.CC.threshold_accel)
     
     controlsState.distanceTraveledSession = float(self.distance_traveled)
     controlsState.distanceTraveledTotal = float(self.distance_traveled_total)
