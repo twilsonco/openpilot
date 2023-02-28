8ce436b20ac6547faca86bb3c98c9f080db305b0
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2023-02-28 11:19:59 -0500

    update my branch (#29)
    * VW Passat NMS custom torque FF
    
    * updated passat FF from 2000 rlog segments
    
    * update passat FF
    
    ---------
    
    Co-authored-by: Tim Wilson <twilsonco@gmail.com>

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -30,12 +30,12 @@ class CarInterface(CarInterfaceBase):
   
   @staticmethod
   def torque_from_lateral_accel_passat_nms(lateral_accel_value, torque_params, lateral_accel_error, lateral_accel_deadzone, friction_compensation, v_ego, g_lat_accel, lateral_jerk_desired):
-    ANGLE_COEF = 4.99998800
-    ANGLE_COEF2 = 0.19246255
-    SPEED_OFFSET = 9.38190347
-    SIGMOID_COEF_RIGHT = 0.20312216
-    SIGMOID_COEF_LEFT = 0.20310046
-    SPEED_COEF = 1.49621668
+    ANGLE_COEF = 4.99998726
+    ANGLE_COEF2 = 0.19249525
+    SPEED_OFFSET = 9.35958809
+    SIGMOID_COEF_RIGHT = 0.20307778
+    SIGMOID_COEF_LEFT = 0.20300700
+    SPEED_COEF = 1.49641419
     x = ANGLE_COEF * (lateral_accel_value) * (40.23 / (max(0.2,v_ego + SPEED_OFFSET))**SPEED_COEF)
     sigmoid = erf(x)
     out = ((SIGMOID_COEF_RIGHT if lateral_accel_value < 0. else SIGMOID_COEF_LEFT) * sigmoid) + ANGLE_COEF2 * lateral_accel_value
