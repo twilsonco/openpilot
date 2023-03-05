29d9e594f22896f6bc9bbc6d67f6e1e1e91726dc
Tim Wilson <twilsonco@gmail.com>
2023-03-04 19:10:13 -0700

    update master script

--- tools/tuning/batch_rlog_acquisition_and_processing.sh
+++ tools/tuning/batch_rlog_acquisition_and_processing.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+
 /usr/bin/env bash /mnt/video/scratch-video/rlog_api/rlogs_copy_from_device_to_local_folder.sh
 
 /usr/bin/env bash /mnt/video/scratch-video/rlog_api/api1.sh
