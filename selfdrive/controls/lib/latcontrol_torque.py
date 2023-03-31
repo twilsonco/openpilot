fc2906e5de81b450781d09c231f8599d2fb137be
Tim Wilson <twilsonco@gmail.com>
2023-03-31 15:59:04 -0600

    still log error scale

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -161,8 +161,8 @@ class LatControlTorque(LatControl):
       setpoint = desired_lateral_accel + low_speed_factor * desired_curvature
       measurement = actual_lateral_accel + low_speed_factor * actual_curvature
       error = setpoint - measurement
-      # error_scale_factor = 2.0
-      # error_scale_factor = 1.0 / (1.0 + min(apply_deadzone(abs(self.max_future_lateral_accel), 0.4) * error_scale_factor, error_scale_factor - 1))
+      error_scale_factor = 2.0
+      error_scale_factor = 1.0 / (1.0 + min(apply_deadzone(abs(self.max_future_lateral_accel), 0.4) * error_scale_factor, error_scale_factor - 1))
       # error *= error_scale_factor
       pid_log.error = error
 
