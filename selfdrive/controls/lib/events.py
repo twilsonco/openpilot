0ae4699c7be243cb176af989eecb00f29c6c3e76
Tim Wilson <twilsonco@gmail.com>
2023-04-17 13:28:55 -0600

    gm autohold
    autohold alert
    
    Missed car.capnp item
    
    Missed import

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -1064,4 +1064,11 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
     ET.NO_ENTRY: NoEntryAlert("LKAS Disabled"),
   },
 
+  EventName.autoHoldActivated: {
+    ET.PERMANENT: Alert(
+    "AutoHolding | Gas to resume",
+    "You can rest your foot now.",
+    AlertStatus.normal, AlertSize.small,
+    Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0.4, .3),
+  },
 }
