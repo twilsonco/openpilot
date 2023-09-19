4b8b5ca205f90a14b58d414828444bfb52f09bb9
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-19 19:51:10 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -797,9 +797,9 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   // paint path edges
   QLinearGradient pe(0, height(), 0, 0);
   if (alwaysOnLateral) {
-    pe.setColorAt(0.0, QColor::fromHslF(28 / 360., 1.0, 0.0, 1.0));
+    pe.setColorAt(0.0, QColor::fromHslF(28 / 360., 1.0, 0.6, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(0 / 360., 1.0, 0.0, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(28 / 360., 1.0, 0.50, 0.1));
+    pe.setColorAt(1.0, QColor::fromHslF(28 / 360., 1.0, 0.00, 0.1));
   } else if (conditionalStatus == 1) {
     pe.setColorAt(0.0, QColor::fromHslF(188 / 360., 0.79, 0.58, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(188 / 360., 0.79, 0.58, 0.5));
