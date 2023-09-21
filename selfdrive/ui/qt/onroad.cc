82be68e48bf218e741dfe3b97c9974102e15d7e5
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-21 08:10:00 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -797,9 +797,9 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   // paint path edges
   QLinearGradient pe(0, height(), 0, 0);
   if (alwaysOnLateral) { // Pink & white
-    pe.setColorAt(0.0, QColor::fromHslF(300 / 360., 1.0, 0.5, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(300 / 360., 1.0, 1.0, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(300 / 360., 1.0, 0.5, 0.1));
+    pe.setColorAt(0.0, QColor::fromHslF(300 / 360.0, 1.0, 0.5, 1.0));   // Start with pink
+pe.setColorAt(0.5, QColor::fromHslF(0.0, 0.0, 1.0, 1.0));           // Transition to white (full saturation and lightness)
+pe.setColorAt(1.0, QColor::fromHslF(0.0, 0.0, 1.0, 1.0));           // Stay at white
   } else if (conditionalStatus == 1) {
     pe.setColorAt(0.0, QColor::fromHslF(188 / 360., 0.79, 0.58, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(188 / 360., 0.79, 0.58, 0.5));
@@ -813,9 +813,9 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
     pe.setColorAt(0.5, QColor::fromHslF(205 / 360., 0.85, 0.56, 0.5));
     pe.setColorAt(1.0, QColor::fromHslF(205 / 360., 0.85, 0.56, 0.1));
   } else if (frogColors) {
-    pe.setColorAt(0.0, QColor::fromHslF(300 / 360., 1.0, 0.5, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(300 / 360., 1.0, 1.0, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(300 / 360., 1.0, 1.0, 0.1));
+    pe.setColorAt(0.0, QColor::fromHslF(0, 0, 1.0, 1.0));
+    pe.setColorAt(0.5, QColor::fromHslF(0, 0, 1.0, 1.0));
+    pe.setColorAt(1.0, QColor::fromHslF(300 / 360., 1.0, 0.5, 1.0));
   } else {
     pe.setColorAt(0.0, QColor::fromHslF(300 / 360., 1.0, 0.5, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(300 / 360., 1.0, 0.5, 0.5));
