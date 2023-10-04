621de3f8b4bb39c1794a7a8e54dd0c600e48493a
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-10-04 18:55:04 -0400

    Update onroad.h

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -194,10 +194,10 @@ protected:
   void drawDriverState(QPainter &painter, const UIState *s);
   inline QColor redColor(int alpha = 255) { return QColor(204, 0, 112, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
-  inline QColor blackColor(int alpha = 255) { return QColor(255, 153, 213, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(255, 163, 224, alpha); }
 
   // FrogPilot colors
-  inline QColor frogColor(int alpha = 255) { return QColor(255, 178, 214, alpha); }
+  inline QColor frogColor(int alpha = 255) { return QColor(255, 163, 224, alpha); }
 
   double prev_draw_t = 0;
   FirstOrderFilter fps_filter;
