taiyangsu/frogpilot

Increase max speed by 5 on short press
Added function to increase the value of the max speed by 5 instead of 1 by taping the "Max" speed icon in the onroad UI.

selfdrive/ui/qt/onroad.cc
@@ -144,13 +144,25 @@ void OnroadWindow::mousePressEvent(QMouseEvent* e) {
 
   bool widgetClicked = false;
 
+  // Change cruise control increments button
+  const QRect maxSpeedRect(1, 1, 350, 350);
+  const bool isMaxSpeedClicked = maxSpeedRect.contains(e->pos());
+
   // Hide speed button
   const QRect speedRect(rect().center().x() - 175, 50, 350, 350);
   const bool isSpeedClicked = speedRect.contains(e->pos());
 
-  if (isSpeedClicked) {
-    speedHidden = !params.getBool("HideSpeed");
-    params.putBool("HideSpeed", speedHidden);
+  if (isMaxSpeedClicked || isSpeedClicked) {
+    // Check if the click was within the max speed area
+    if (isMaxSpeedClicked) {
+      reverseCruiseIncrease = !params.getBool("ReverseCruiseIncrease");
+      params.putBool("ReverseCruiseIncrease", reverseCruiseIncrease);
+      paramsMemory.putBool("FrogPilotTogglesUpdated", true);
+    // Check if the click was within the speed text area
+    } else {
+      speedHidden = !params.getBool("HideSpeed");
+      params.putBool("HideSpeed", speedHidden);
+    }
     widgetClicked = true;
   // If the click wasn't for anything specific, change the value of "ExperimentalMode"
   } else if (scene.experimental_mode_via_wheel && (scene.enabled || previouslyEnabled) && e->pos() != timeoutPoint) {
@@ -415,6 +427,9 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   if (params.getBool("HideSpeed")) {
     speedHidden = true;
   }
+  if (params.getBool("ReverseCruiseIncrease")) {
+    reverseCruiseIncrease = true;
+  }
 
   // Load miscellaneous images
 
@@ -595,7 +610,11 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
   int bottom_radius = has_eu_speed_limit ? 100 : 32;
 
   QRect set_speed_rect(QPoint(60 + (default_size.width() - set_speed_size.width()) / 2, 45), set_speed_size);
-  p.setPen(QPen(whiteColor(75), 6));
+  if (reverseCruiseIncrease) {
+    p.setPen(QPen(QColor(0, 150, 255), 6));
+  } else {
+    p.setPen(QPen(whiteColor(75), 6));
+  }
   p.setBrush(blackColor(166));
   drawRoundedRect(p, set_speed_rect, top_radius, top_radius, bottom_radius, bottom_radius);
 

twilsonco/openpilot

selfdrive/ui/qt/onroad.cc
@@ -139,13 +139,25 @@ void OnroadWindow::mousePressEvent(QMouseEvent* e) {
 
   bool widgetClicked = false;
 
+  // Change cruise control increments button
+  const QRect maxSpeedRect(7, 25, 225, 225);
+  const bool isMaxSpeedClicked = maxSpeedRect.contains(e->pos());
+
   // Hide speed button
   const QRect speedRect(rect().center().x() - 175, 50, 350, 350);
   const bool isSpeedClicked = speedRect.contains(e->pos());
 
-  if (isSpeedClicked) {
-    speedHidden = !params.getBool("HideSpeed");
-    params.putBoolNonBlocking("HideSpeed", speedHidden);
+  if (isMaxSpeedClicked || isSpeedClicked) {
+    // Check if the click was within the max speed area
+    if (isMaxSpeedClicked) {
+      reverseCruiseIncrease = !params.getBool("ReverseCruiseIncrease");
+      params.putBoolNonBlocking("ReverseCruiseIncrease", reverseCruiseIncrease);
+      paramsMemory.putBoolNonBlocking("FrogPilotTogglesUpdated", true);
+    // Check if the click was within the speed text area
+    } else {
+      speedHidden = !params.getBool("HideSpeed");
+      params.putBoolNonBlocking("HideSpeed", speedHidden);
+    }
     widgetClicked = true;
   // If the click wasn't for anything specific, change the value of "ExperimentalMode"
   } else if (scene.experimental_mode_via_wheel && e->pos() != timeoutPoint) {
@@ -417,6 +429,9 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   if (params.getBool("HideSpeed")) {
     speedHidden = true;
   }
+  if (params.getBool("ReverseCruiseIncrease")) {
+    reverseCruiseIncrease = true;
+  }
 
   // Load miscellaneous images
 
@@ -597,7 +612,11 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
   int bottom_radius = has_eu_speed_limit ? 100 : 32;
 
   QRect set_speed_rect(QPoint(60 + (default_size.width() - set_speed_size.width()) / 2, 45), set_speed_size);
-  p.setPen(QPen(whiteColor(75), 6));
+  if (reverseCruiseIncrease) {
+    p.setPen(QPen(QColor(0, 150, 255), 6));
+  } else {
+    p.setPen(QPen(whiteColor(75), 6));
+  }
   p.setBrush(blackColor(166));
   drawRoundedRect(p, set_speed_rect, top_radius, top_radius, bottom_radius, bottom_radius);
 