82fb557430921b1cc6e6061e09f927f7c7313049
Tim Wilson <twilsonco@gmail.com>
2023-11-29 12:06:35 -0700

    fix rr.sh sudo

--- rr.sh
+++ rr.sh
@@ -2,5 +2,5 @@
 # rr - rebootless restart
 ## old version
 # tmux kill-session -t comma; rm -f /tmp/safe_staging_overlay.lock; tmux new -s comma -d "/data/openpilot/launch_openpilot.sh"
-systemctl restart comma
+sudo systemctl restart comma
 echo "Restarting openpilot... Exit code $?"
