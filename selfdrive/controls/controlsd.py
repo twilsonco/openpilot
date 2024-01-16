dd83f0858afcbe66aec5b766d1e758df72ea9d14
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-16 14:46:48 -0700

    January 16th, 2024 Patch for FrogPilot v0.9.6
    - Fixed an issue that was causing a "Controls Process Lagging" error for some users

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -964,12 +964,11 @@ class Controls:
       if self.CP.openpilotLongitudinalControl:
         if self.conditional_experimental_mode:
           self.experimental_mode = self.sm['frogpilotLongitudinalPlan'].conditionalExperimental
-          time.sleep(0.01)
         else:
           self.experimental_mode = self.params.get_bool("ExperimentalMode") or self.params_memory.get_bool("SLCExperimentalMode")
-          time.sleep(0.1)
       if self.CP.notCar:
         self.joystick_mode = self.params.get_bool("JoystickDebugMode")
+      time.sleep(0.1)
 
       # Update FrogPilot parameters
       if self.params_memory.get_bool("FrogPilotTogglesUpdated"):
