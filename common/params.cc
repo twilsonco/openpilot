b879617f33a6b82991d86bcbedbd40a9d9aa30bf
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    Navigate on Chill Mode
    Co-Authored-By: garrettpall <76917194+garrettpall@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -291,6 +291,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MTSCEnabled", PERSISTENT},
     {"MTSCLimit", PERSISTENT},
     {"MuteOverheated", PERSISTENT},
+    {"NavChill", PERSISTENT},
     {"NoLogging", PERSISTENT},
     {"PathEdgeWidth", PERSISTENT},
     {"PathWidth", PERSISTENT},
