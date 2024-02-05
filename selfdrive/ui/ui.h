97779f093f06820ee1298c85dadd051319be167c
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    Rotating steering wheel in onroad UI
    Added toggle to rotate the steering wheel in the top right corner of the onroad UI.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -203,6 +203,7 @@ typedef struct UIScene {
   bool reverse_cruise;
   bool reverse_cruise_ui;
   bool road_name_ui;
+  bool rotating_wheel;
   bool show_driver_camera;
   bool tethering_enabled;
   bool turn_signal_left;
@@ -229,6 +230,7 @@ typedef struct UIScene {
   int desired_follow;
   int obstacle_distance;
   int obstacle_distance_stock;
+  int steering_angle_deg;
   int stopped_equivalence;
 
   QPolygonF track_adjacent_vertices[6];
