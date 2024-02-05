f880b413db25935ab25a4939a1a41509451bd9ae
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    Smoother braking behind lead
    Added toggle to smooth out the braking behavior when approaching a slower lead vehicle.

--- common/params.cc
+++ common/params.cc
@@ -344,6 +344,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ShowStorageLeft", PERSISTENT},
     {"ShowStorageUsed", PERSISTENT},
     {"Sidebar", PERSISTENT},
+    {"SmoothBraking", PERSISTENT},
     {"StandardFollow", PERSISTENT},
     {"StandardJerk", PERSISTENT},
     {"StockTune", PERSISTENT},
