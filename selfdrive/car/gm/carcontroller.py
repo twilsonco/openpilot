dd3b69e9bfc011de10836046dfda6e708bacbb79
Tim Wilson <twilsonco@gmail.com>
2024-02-22 23:29:28 -0700

    fix cruise not sending gas

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -199,7 +199,7 @@ class CarController:
               at_full_stop = at_full_stop and not resume
 
             # GasRegenCmdActive needs to be 1 to avoid cruise faults. It describes the ACC state, not actuation
-            can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, CanBus.POWERTRAIN, self.apply_gas, idx, CC.enabled and CS.out.cruiseState.enabled, at_full_stop))
+            can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, CanBus.POWERTRAIN, self.apply_gas, idx, CC.enabled, at_full_stop))
             can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, friction_brake_bus, self.apply_brake,
                                                                 idx, CC.enabled, near_stop, at_full_stop, self.CP))
             CS.autoHoldActivated = False
