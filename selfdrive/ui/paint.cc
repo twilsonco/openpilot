f175e8301edd82743f3768d12e3cf43f949291bf
devimauz <cs.deviance@gmail.com>
2024-11-05 05:39:17 +0100

    Refactor UI code for stopping line

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1918,18 +1918,13 @@ void DrawApilot::drawLeadApilot(const UIState* s) {
     static float tf_distance_x = 0.0, tf_distance_y = 0.0;
     tf_distance_x = tf_distance_x * 0.9 + s->tf_distance_point.x() * 0.1;
     tf_distance_y = tf_distance_y * 0.9 + s->tf_distance_point.y() * 0.1;
+
     nvgBeginPath(s->vg);
-    nvgCircle(s->vg, tf_distance_x, tf_distance_y, 20 / 2);
-    nvgFillColor(s->vg, COLOR_RED);
-    nvgFill(s->vg);
-    nvgBeginPath(s->vg);
-    nvgCircle(s->vg, tf_distance_x - 60, tf_distance_y, 20 / 2);
-    nvgFillColor(s->vg, COLOR_RED);
-    nvgFill(s->vg);
-    nvgBeginPath(s->vg);
-    nvgCircle(s->vg, tf_distance_x + 60, tf_distance_y, 20 / 2);
-    nvgFillColor(s->vg, COLOR_RED);
-    nvgFill(s->vg);
+    nvgMoveTo(s->vg, tf_distance_x - 100, tf_distance_y);
+    nvgLineTo(s->vg, tf_distance_x + 100, tf_distance_y);
+    nvgStrokeColor(s->vg, COLOR_WHITE);
+    nvgStrokeWidth(s->vg, 5);
+    nvgStroke(s->vg);
 
     drawPathEnd(s, x, y, path_x, path_y, path_width);
     drawGapInfo(s, x, y);
