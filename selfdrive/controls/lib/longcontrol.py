59edcfbed437344a79ac62cc586a1854178e7f3e
Tim Wilson <twilsonco@gmail.com>
2025-04-17 10:39:44 -0600

    long jerk ff
    simpler

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -69,6 +69,10 @@ class LongControl:
     self.longitudinalTuningKf = 1.0
     self.startAccelApply = 0.0
     self.stopAccelApply = 0.0
+    
+    # jerf ff
+    self.jerk_ff_kf = 1.0
+    self.jerk_ff_t_delay = 0.0 # seconds
 
   def reset(self, v_pid):
     """Reset PID controller and change setpoint"""
@@ -116,6 +120,10 @@ class LongControl:
       a_target = min(a_target_lower, a_target_upper)
 
       v_target_1sec = interp(self.CP.longitudinalActuatorDelayUpperBound + t_since_plan + 1.0, ModelConstants.T_IDXS[:CONTROL_N], speeds)
+      
+      # jerk ff
+      jerk_target = interp(t_since_plan + self.jerk_ff_t_delay, ModelConstants.T_IDXS[:CONTROL_N], long_plan.jerks)
+      a_target += self.jerk_ff_kf * jerk_target
     else:
       v_target = 0.0
       v_target_now = 0.0
